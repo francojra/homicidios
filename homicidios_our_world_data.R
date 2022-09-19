4bdbc79ec329af1b32afa291e5032825ae203135
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-18 23:04:58 -0300

    Add files via upload

---
+++ homicidios_our_world_data.R
@@ -0,0 +1,35 @@
+
+# Homicídios -------------------------------------------------------------------------------------------------------------------------------
+# Autoria do script: Jeanne Franco ---------------------------------------------------------------------------------------------------------
+# Data: 18/09/22 ---------------------------------------------------------------------------------------------------------------------------
+# Referência: https://ourworldindata.org/homicides -----------------------------------------------------------------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Esse artigo trata de homicídios intecionais que são definidos como "uma
+### morte ilegal infligida deliberadamente a uma pessoa por outra".
+
+### Homicídio se refere a uma violência interpessoal. Mortes civis e 
+### militares durante guerras interestaduais, guerras civis e genocídios 
+### não são contados como homicídios.
+
+### Menos que 1% das mortes no mundo são por causa de homicídio, mas em alguns
+### países a porcentagem alcança 10%. Globalmente, 0.7% das mortes em 2019
+### foram resultantes de homicídio.
+
+### A porcentagem de mortes varia em todo mundo. Existem grandes diferenças
+### entre países e regiões.
+
+### Em torno dos países da Europa ocidental, por exemplo, menos que 0.1% de 
+### mortes resultaram de homicídio. Já em grande parte da Europa oriental,
+### norte da África, Ásia e Oceania, foi menos que 0.5%. Nos Estados Unidos
+### alcançou 0.6%.
+
+### Em alguns países a porcentagem é muito maior. Nós vemos altas taxas em
+### países da América Latina. Mais que 7% de mortes em El Salvador foram
+### por homicídios, mais que 6% na Guatemala e 5% na Venezuela.
+
+
+
+
+
